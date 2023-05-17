b1cc5840a7b43f0b9126e079ce20b8b99f16059d
renkepoonam <130247671+renkepoonam@users.noreply.github.com>
2023-05-17 16:30:58 +0530

    Reimbursement Code

---
+++ OnlineShoppingProject/src/main/java/com/ecommerce/onlineshopping/repository/ReimbursementRepository.java
@@ -0,0 +1,13 @@
+package com.ecommerce.onlineshopping.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.onlineshopping.model.Reimbursement;
+
+@Repository
+public interface ReimbursementRepository extends JpaRepository<Reimbursement, Integer> {
+	
+	
+
+}
---
+++ OnlineShoppingProject/src/main/java/com/ecommerce/onlineshopping/service/ReimbursementService.java
@@ -0,0 +1,12 @@
+package com.ecommerce.onlineshopping.service;
+
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.onlineshopping.model.Reimbursement;
+
+
+public interface ReimbursementService {
+
+	public Reimbursement addReimbursementDetails(Reimbursement reimbursement);
+
+}
